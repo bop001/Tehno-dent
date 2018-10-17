8deb924f796c96b79d3edfb539ab108ceaa77620
R4tm4n <1mar4tm4n@gmail.com>
2018-10-17 14:11:53 +0300

    Modify sass ui-derectory

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,11 @@ var gulp         = require('gulp'),
     cssnano      = require('gulp-cssnano'),
     rename       = require('gulp-rename'),
     del          = require('del'),
-    imagemin     = require('gulp-imagemin'),
-    pngquant     = require('imagemin-pngquant'),
     cache        = require('gulp-cache'),
     autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
-    return gulp.src('app/sass/**/*')
+    return gulp.src('app/sass/*.sass')
       .pipe(sass())
       .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
       .pipe(gulp.dest('app/css/'))
@@ -22,7 +20,9 @@ gulp.task('sass', function(){
 
 gulp.task('pug', function(){
   return gulp.src('app/pug/**/*.pug')
-    .pipe(pug())
+    .pipe(pug({
+        pretty: true
+    }))
     .pipe(gulp.dest('app'))
     .pipe(browserSync.reload({stream: true}));
 });
@@ -38,8 +38,14 @@ gulp.task('browser-sync', function(){
 gulp.task('css-libs', function(){
   return gulp.src([
     'node_modules/owl.carousel/dist/assets/owl.carousel.css',
-    'node_modules/fancybox/dist/css/jquery.fancybox.css',
-    'node_modules/hamburgers/dist/hamburgers.css'
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+    'node_modules/hamburgers/dist/hamburgers.css',
+    'node_modules/jquery.mmenu/dist/jquery.mmenu.all.css',
+    'node_modules/nouislider/distribute/nouislider.css',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+    'node_modules/responsive-tabs/css/jquery.responsive-tabs.css',
+    'app/vendor/css/preloader.css',
+    'node_modules/slick-carousel/slick/slick.css'
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssnano())
@@ -49,8 +55,14 @@ gulp.task('css-libs', function(){
 gulp.task('scripts', function(){
   return gulp.src([
       'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/fancybox/dist/js/jquery.fancybox.js',
-      'node_modules/owl.carousel/dist/owl.carousel.js'
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/owl.carousel/dist/owl.carousel.js',
+      'node_modules/masonry-layout/dist/masonry.pkgd.js',
+      'node_modules/jquery.mmenu/dist/jquery.mmenu.all.js',
+      'node_modules/nouislider/distribute/nouislider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      "node_modules/responsive-tabs/js/jquery.responsiveTabs.js",
+      'node_modules/slick-carousel/slick/slick.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -61,18 +73,7 @@ gulp.task('clean', function() {
   return del.sync('dist');
 });
 
-gulp.task('img', function() {
-  return gulp.src('app/img/**/*')
-    .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    })))
-    .pipe(gulp.dest('dist/img'));
-});
-
-gulp.task('build', ['clean', 'img', 'pug', 'sass', 'scripts'], function() {
+gulp.task('build', ['clean', 'pug', 'sass', 'scripts'], function() {
 
   var buildCss = gulp.src('app/css/**/*.css')
     .pipe(gulp.dest('dist/css'))
@@ -83,10 +84,11 @@ gulp.task('build', ['clean', 'img', 'pug', 'sass', 'scripts'], function() {
   var buildJs = gulp.src('app/js/**/*')
     .pipe(gulp.dest('dist/js'));
 
-
   var buildHtml = gulp.src('app/*.html')
     .pipe(gulp.dest('dist'));
 
+  var buildImg = gulp.src('app/img/**/*')
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('clear', function (callback) {
