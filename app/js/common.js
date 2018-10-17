afdb70ffbaa4707899643e4ef1cd1d9f0838aa2c
R4tm4n <1mar4tm4n@gmail.com>
2018-10-17 14:04:40 +0300

    Add preloader and update package.json and gulpfile.js

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,13 @@
 $( document ).ready(function() {
-
+    preloader();//Fade preloader when page loaded
 
 });
+
+
+// functions -------------------------------------------
+
+function preloader () {
+    $(window).on('load', function () {
+        $('.loader-outer').fadeOut();
+    });
+}
